8ec741440fad47e3529623c5afcca85d463adfb6
Enrich <76450269+Projetmaths@users.noreply.github.com>
2020-12-24 14:52:47 +0100

    fix project.py + index.html

--- Projet.py
+++ Projet.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('file.html')
+    return render_template('index.html ')
 
 @app.route('/SomeFunction')
 def SomeFunction():
