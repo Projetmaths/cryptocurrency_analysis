e53aca887007b8140a9812a899b787aae76598eb
Enrich <enrich.flejo+projetmaths@gmail.com>
2020-12-28 21:45:27 +0100

    add js and css bootstrap in index.html + update jquery to 3.5.1 from 2.1.3

--- static/javascript/fonctions.js
+++ static/javascript/fonctions.js
@@ -1,1 +1,1 @@
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"> </script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"> </script>
