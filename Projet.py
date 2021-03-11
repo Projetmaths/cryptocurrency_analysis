3a829dcf208a0f8f293fc3c5855df57f0e439c08
Enrich <76450269+Projetmaths@users.noreply.github.com>
2021-03-11 16:11:24 +0100

    dashboard and 404.html

--- Projet.py
+++ Projet.py
@@ -67,7 +67,11 @@ def index():
     
     return render_template('index.html', data=importhtml, incr=incr)
 
-
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+    
 @app.route('/chart')
 def chart():
     labels = []
