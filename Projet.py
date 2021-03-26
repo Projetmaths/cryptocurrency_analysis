e0c8212ee9059630607f346a8147441f94b17001
Enrich <enrich.flejo+projetmaths@gmail.com>
2021-03-26 21:06:41 +0100

    continue task in previous commit

--- Projet.py
+++ Projet.py
@@ -46,8 +46,26 @@ def chart():
         csvfile.close()
     labels = []
     data = []
-    prix = []
-    date = []
+    prix_btc = []
+    date_btc = []
+    prix_ada = []
+    date_ada = []
+    prix_bch = []
+    date_bch = []
+    prix_bnb = []
+    date_bnb = []
+    prix_doge = []
+    date_doge = []
+    prix_dot = []
+    date_dot = []
+    prix_eth = []
+    date_eth = []
+    prix_link = []
+    date_link = []
+    prix_ltc = []
+    date_ltc = []
+    prix_luna = []
+    date_luna = []
     name = []
     symbol = []    
     for i in range(0, lim):
@@ -82,12 +100,48 @@ def chart():
                 
                 
                 if name[i] == "Bitcoin":
-                    prix.append(val2)
-                    date.append(val3)
+                    prix_btc.append(val2)
+                    date_btc.append(val3)
+                    
+                if name[i] == "Cardano":
+                    prix_ada.append(val2)
+                    date_ada.append(val3)
+                    
+                if name[i] == "Bitcoin Cash":
+                    prix_bch.append(val2)
+                    date_bch.append(val3)
+                
+                if name[i] == "Binance Coin":
+                    prix_bnb.append(val2)
+                    date_bnb.append(val3)
+                
+                if name[i] == "Dogecoin":
+                    prix_doge.append(val2)
+                    date_doge.append(val3)
+                
+                if name[i] == "Polkadot":
+                    prix_doge.append(val2)
+                    date_doge.append(val3)
+
+                if name[i] == "Ethereum":
+                    prix_eth.append(val2)
+                    date_eth.append(val3)
+                
+                if name[i] == "Chainlink":
+                    prix_link.append(val2)
+                    date_link.append(val3)
+                
+                if name[i] == "Litecoin":
+                    prix_ltc.append(val2)
+                    date_ltc.append(val3)
+                
+                if name[i] == "Terra":
+                    prix_luna.append(val2)
+                    date_luna.append(val3)
 
                 count+=1         
                 
-    return render_template('chart.html', labels = labels, data = data, prix = prix, date = date, lim=lim, count=count, name=name, symbol=symbol)   
+    return render_template('chart.html', labels = labels, data = data,  lim=lim, count=count, name=name, symbol=symbol, prix_btc = prix_btc, date_btc = date_btc, prix_ada = prix_ada, date_ada = date_ada , prix_bch = prix_bch, date_bch = date_bch, prix_bnb = prix_bnb, date_bnb = date_bnb, prix_doge = prix_doge, date_doge = date_doge,prix_dot = prix_dot, date_dot = date_dot,prix_eth = prix_eth, date_eth = date_eth,prix_link = prix_link, date_link = date_link,prix_ltc = prix_ltc, date_ltc = date_ltc,prix_luna = prix_luna, date_luna = date_luna)   
     
 @app.errorhandler(404)
 def page_not_found(e):
